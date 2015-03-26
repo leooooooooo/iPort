10343e516f8345f55f9fcada5baeb1ea4a4dfdd6
张超 <zhang62220@gmail.com>
2015-03-26 14:42:36 +0800

    1

---
+++ iPort/iPort/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  iPort
+//
+//  Created by leo on 15/3/26.
+//  Copyright (c) 2015年 leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
