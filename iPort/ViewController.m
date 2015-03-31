fbf167151f3851f017080d41b3610e0be8128814
张超 <zhang62220@gmail.com>
2015-03-31 16:38:45 +0800

    AppIcon

--- iPort/ViewController.m
+++ iPort/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIImageView *test = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 29, 29)];
+    test.image = [UIImage imageNamed:@"29x29"];
+    [self.view addSubview:test];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
